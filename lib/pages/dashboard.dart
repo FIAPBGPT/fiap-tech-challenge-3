ee6424c96c3b2599d593d1556fe66bea5dad7cf5
Maria Luiza Pereira <maluupereiraa@gmail.com>
2025-02-24 22:46:22 -0300

    Button

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -3,6 +3,8 @@ import 'package:lottie/lottie.dart'; // Import Lottie package
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import '../providers/transaction.provider.dart';
+import '../widgets/button.dart';
+import '../utils/constants.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
@@ -15,6 +17,104 @@ class DashboardPage extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            Text(
+              'Welcome to My App!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            //  CustomButton(
+            //   text: 'Elevated Button',
+            //   onPressed: () => print('Elevated Button Pressed'),
+            //   type: ButtonType.elevated,
+            // ),
+            // SizedBox(height: 10),
+
+            // CustomButton(
+            //   text: 'Outlined Button',
+            //   onPressed: () => print('Outlined Button Pressed'),
+            //   type: ButtonType.outlined,
+            //   color: Colors.red,
+            // ),
+            // SizedBox(height: 10),
+
+            // CustomButton(
+            //   text: 'Text Button',
+            //   onPressed: () => print('Text Button Pressed'),
+            //   type: ButtonType.text,
+            //   color: Colors.green,
+            // ),
+
+            Wrap(
+              spacing: 5, // Space between buttons
+              runSpacing: 5, // Space between lines
+              children: [
+                CustomButton(
+                  text: '',
+                  onPressed: () => print('BTN EDIT'),
+                  type: ButtonType.icon,
+                  icon: Icons.edit,
+                  color: AppConstants.baseBlueBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: '',
+                  onPressed: () => print('BTN DELETE'),
+                  type: ButtonType.icon,
+                  icon: Icons.delete,
+                  color: AppConstants.baseBlueBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Concluir transação',
+                  onPressed: () => print('Elevated Button Pressed'),
+                  type: ButtonType.elevated,
+                  color: AppConstants.baseBlueBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Botão laranja',
+                  onPressed: () => print('Elevated Button Pressed'),
+                  type: ButtonType.elevated,
+                  color: AppConstants.baseOrangeBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Botão outlined laranja',
+                  onPressed: () => print('Elevated Button Pressed'),
+                  type: ButtonType.outlined,
+                  color: AppConstants.baseOrangeBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Botão verde',
+                  onPressed: () => print('Elevated Button Pressed'),
+                  type: ButtonType.elevated,
+                  color: AppConstants.baseGreenBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Botão verde outlined',
+                  onPressed: () => print('Outlined Button Pressed'),
+                  type: ButtonType.outlined,
+                  color: AppConstants.baseGreenBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Botão preto',
+                  onPressed: () => print('Elevated Button Pressed'),
+                  type: ButtonType.elevated,
+                  color: AppConstants.baseBlackBytebank,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  text: 'Botão preto outlined',
+                  onPressed: () => print('Outlined Button Pressed'),
+                  type: ButtonType.outlined,
+                  color: AppConstants.baseBlackBytebank,
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+
             // Add Lottie Animation
             Lottie.asset(
               'lib/assets/animations/financial_animation.json', // Path to your JSON file
