d94c077b13856cf4b796af0fbc99f3bc794da687
Thiago F. Martins <thiagofm33@gmail.com>
2025-03-03 19:08:24 -0300

    Updated `lib/routes.dart`

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,5 @@
 class Routes {
   static const String signUp = '/sign-up';
   static const String signIn = '/sign-in';
-  static const String imageView = '/image-view';
-  static const String imageGallery = '/image-gallery';
+  static const String transactions = '/transactions';
 }
