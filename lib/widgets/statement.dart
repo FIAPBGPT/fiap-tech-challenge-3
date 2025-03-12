0a1553594c98e7a313c4f2f67d621fcee0d43ab1
Thiago F. Martins <thiagofm33@gmail.com>
2025-03-12 20:32:57 -0300

    Implemented the statement export feature

--- lib/widgets/statement.dart
+++ lib/widgets/statement.dart
@@ -1,12 +1,16 @@
-import 'dart:ffi';
-
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:bytebank/config/dio_client.dart';
 import 'package:bytebank/utils/constants.dart';
 import 'package:bytebank/widgets/button.dart';
 import 'package:bytebank/widgets/statement/statement_item.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:open_file_plus/open_file_plus.dart';
+
+const String firebaseAppName = 'tech-challenge-fase-3-abc';
 
 class Statement extends StatefulWidget {
   const Statement({super.key});
@@ -17,12 +21,17 @@ class Statement extends StatefulWidget {
 
 class _StatementState extends State<Statement> {
   final DioClient _dioClient = DioClient();
+  final TextStyle footerLinksStyle = TextStyle(color: Colors.green);
+  final String statementFilePath = 'files/latest_statement.txt';
+
   List<dynamic> _transactions = [];
   List<dynamic> _filteredTransactions = [];
   int _sortDirection = 1;
   int _filterMode = 0;
   IconData _sortingIcon = Icons.arrow_downward;
   IconData _filteringIcon = Icons.sync_alt;
+  bool? _loading;
+  bool _fileUploaded = false;
 
   Future<List<dynamic>> _loadTransactions() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -90,6 +99,77 @@ class _StatementState extends State<Statement> {
     });
   }
 
+  Future<void> _uploadFile() async {
+    String csv = 'Transaction Type,Amount,Description,Date\n';
+
+    _filteredTransactions.forEach((transaction) {
+      final line = [
+        transaction['transactionType'],
+        transaction['amount'],
+        transaction['description'],
+        transaction['date'],
+      ];
+
+      csv = '$csv${line.join(',')}\n';
+    });
+
+    try {
+      final Reference storageRef = FirebaseStorage.instance.ref();
+      final fileRef = storageRef.child(statementFilePath);
+      final uploadTask = fileRef.putString(csv);
+
+      setState(() => _loading = true);
+
+      final taskSnapshot = await uploadTask.whenComplete(() => null);
+      await taskSnapshot.ref.getDownloadURL();
+
+      setState(() {
+        _loading = false;
+        _fileUploaded = true;
+      });
+
+      print('Arquivo enviado!');
+    } catch (e) {
+      print('Falha ao fazer upload do arquivo: $e');
+    }
+  }
+
+  Future<void> _downloadFile() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final Reference storageRef = FirebaseStorage.instance.ref();
+      final fileRef = storageRef.child(statementFilePath);
+      final fileData = await fileRef.getData();
+
+      String downloadPath = '${directory.path}/latest_statement.txt';
+      File downloadFile = File(downloadPath);
+
+      downloadFile.writeAsString(String.fromCharCodes(fileData!));
+
+      OpenFile.open(downloadPath);
+    } catch (e) {
+      print('Falha ao fazer baixar do arquivo: $e');
+    }
+  }
+
+  Future<void> _deleteFile() async {
+    try {
+      final Reference storageRef = FirebaseStorage.instance.ref();
+      final fileRef = storageRef.child(statementFilePath);
+
+      setState(() => _loading = true);
+
+      fileRef.delete();
+
+      setState(() {
+        _loading = false;
+        _fileUploaded = false;
+      });
+    } catch (e) {
+      print('Falha ao fazer apagar o arquivo: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card.filled(
@@ -147,7 +227,49 @@ class _StatementState extends State<Statement> {
                   ],
                 );
               },
-            )
+            ),
+
+            // Space
+            SizedBox(height: 15),
+
+            Builder(builder: (context) {
+              List<TextButton> buttons = [];
+
+              if (_loading == true) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: AppConstants.baseGreenBytebank,
+                    strokeWidth: 3,
+                  ),
+                );
+              }
+
+              if (!_fileUploaded) {
+                buttons = [
+                  TextButton(
+                    onPressed: _uploadFile,
+                    child: Text('Exportar', style: footerLinksStyle),
+                  )
+                ];
+              } else {
+                buttons = [
+                  TextButton(
+                    onPressed: _downloadFile,
+                    child: Text('Baixar', style: footerLinksStyle),
+                  ),
+                  TextButton(
+                    onPressed: _deleteFile,
+                    child: Text('Apagar arquivo', style: footerLinksStyle),
+                  )
+                ];
+              }
+
+              // Footer Text Buttons
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: buttons,
+              );
+            })
           ],
         ),
       ),
