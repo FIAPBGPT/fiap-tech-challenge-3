1b300cf6dc298220e3781a8cf8c199cfaaa99db4
Maria Luiza Pereira <maluupereiraa@gmail.com>
2025-03-16 23:04:57 -0300

    Join_Parts

--- lib/pages/main_dashboard.dart
+++ lib/pages/main_dashboard.dart
@@ -1,9 +1,16 @@
 import 'package:bytebank/config/auth_service.dart';
 import 'package:bytebank/forms/transaction-form.dart';
+import 'package:bytebank/pages/chart.dart';
 import 'package:bytebank/pages/weather_screen.dart';
+import 'package:bytebank/utils/constants.dart';
+import 'package:bytebank/utils/constants_keys.dart';
+import 'package:bytebank/widgets/balance_card.dart';
+import 'package:bytebank/widgets/button.dart';
+import 'package:bytebank/widgets/card.dart';
 import 'package:bytebank/widgets/menu/drawer.dart';
 import 'package:bytebank/widgets/statement.dart';
 import 'package:bytebank/widgets/transaction_card.dart';
+import 'package:bytebank/widgets/weather-widget.dart';
 import 'package:flutter/material.dart';
 
 class MainDashboard extends StatelessWidget {
@@ -46,11 +53,13 @@ class MainDashboard extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                Container(
-                  height: 400,
-                  width: double.infinity,
-                  child: WeatherScreen(),
-                ),
+                BalanceCard(),
+
+                /**
+                   * ---------------------------
+                   * The Transaction Widget
+                   * ---------------------------
+                   */
                 Container(
                   height: 400,
                   width: double.infinity,
@@ -75,7 +84,53 @@ class MainDashboard extends StatelessWidget {
                   height: 600,
                   width: double.infinity,
                   child: Statement(),
-                )
+                ),
+
+                /**
+                   * ---------------------------
+                   * The Weather Widget
+                   * ---------------------------
+                   */
+                Container(
+                  height: 350,
+                  width: double.infinity,
+                  child: WeatherWidget(
+                    apiKey: ConstantsKeys.MY_APY_KEY,
+                  ),
+                ),
+                /**
+                   * ---------------------------
+                   * The Credit Card Widget
+                   * ---------------------------
+                   */
+                Card(
+                  color: AppConstants.cardLightBackground,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      width: double.infinity,
+                      child: CreditCardWidget()),
+                ),
+                Card(
+                  color: AppConstants.cardLightBackground,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    width: double.infinity,
+                    child: CustomButton(
+                      text: 'Visualizar Gráficos',
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ResumoTransacoesPage()),
+                      ),
+                      type: ButtonType.outlined,
+                      color: AppConstants.baseBlackBytebank,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
